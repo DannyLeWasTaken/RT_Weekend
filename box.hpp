ec1495a4014812eebdeb6c1d693c57b8cc075de1
DannyLeWasTaken <DannyLeWasTaken@protonmail.com>
2022-10-08 23:49:46 -0400

    Hello, Cornell Box!

--- box.hpp
+++ box.hpp
@@ -5,10 +5,45 @@
 #ifndef UNTITLED_BOX_HPP
 #define UNTITLED_BOX_HPP
 
+#include "rtweekend.hpp"
+#include "aarect.hpp"
+#include "hittable_list.hpp"
 
-class box {
+class box : public hittable {
+public:
+    box() {}
+    box(const glm::dvec3& p0, const glm::dvec3& p1, shared_ptr<material> ptr);
 
+    virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+    virtual bool bounding_box(double time0, double time1, aabb& output_box) const override {
+        output_box = aabb(box_min, box_max);
+        return true;
+    }
+
+public:
+    glm::dvec3 box_min;
+    glm::dvec3 box_max;
+    hittable_list sides;
 };
+
+box::box(const glm::dvec3& p0, const glm::dvec3& p1, shared_ptr<material>ptr) {
+    box_max = p1;
+    box_min = p0;
+
+    sides.add(make_shared<xy_rect>(p0.x, p1.x, p0.y, p1.y, p1.z, ptr));
+    sides.add(make_shared<xy_rect>(p0.x, p1.x, p0.y, p1.y, p0.z, ptr));
+
+    sides.add(make_shared<xz_rect>(p0.x, p1.x, p0.z, p1.z, p1.y, ptr));
+    sides.add(make_shared<xz_rect>(p0.x, p1.x, p0.z, p1.z, p0.y, ptr));
+
+    sides.add(make_shared<yz_rect>(p0.y, p1.y, p0.z, p1.z, p1.x, ptr));
+    sides.add(make_shared<yz_rect>(p0.y, p1.y, p0.z, p1.z, p0.x, ptr));
+}
+
+bool box::hit(const ray &r, double t_min, double t_max, hit_record &rec) const {
+    return sides.hit(r, t_min, t_max, rec);
+}
 
 
 #endif //UNTITLED_BOX_HPP
--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "glmutil.hpp"
 #include "moving_sphere.hpp"
 #include "aarect.hpp"
+#include "box.hpp"
 
 
 glm::dvec3 ray_color(const ray& r, const glm::dvec3 background, const hittable_list& world, int depth) {
@@ -47,6 +48,16 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(glm::dvec3(0, 0, 0), glm::dvec3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, glm::dvec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(glm::dvec3(0,0,0), glm::dvec3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, glm::dvec3(130,0,65));
+    objects.add(box2);
 
     return objects;
 }
@@ -202,8 +213,8 @@ int main() {
         case 6:
             world = cornell_box();
             aspect_ratio = 1.0;
-            image_width = 600;
-            samples_per_pixel = 600;
+            image_width = 300;
+            samples_per_pixel = 32;
             background = glm::dvec3(0,0,0);
             lookFrom = glm::dvec3(278, 278, -800);
             lookAt = glm::dvec3(278, 278, 0);
